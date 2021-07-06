a751bade76e81bb82545a13778442588183da83a
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-06 23:49:16 +0900

    Raycast 활용 연습

---
+++ Assets/raycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        raycasting();
+    }
+
+
+    void raycasting(){
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward *1000);
+        if(Physics.Raycast(transform.position, forward, out hit)){
+            Debug.Log("HIT");
+        }
+        Debug.DrawRay(transform.position, forward, Color.blue)
+    }
+}
