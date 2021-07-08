7ea2722f571296a2aec9d093924096fc24ab886b
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-08 23:27:36 +0900

    raycast 버튼 선택 기능 추가

--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class raycast : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class raycast : MonoBehaviour
         RaycastHit hit;
         Vector3 forward = transform.TransformDirection(Vector3.forward *1000);
         if(Physics.Raycast(transform.position, forward, out hit)){
+            hit.transform.GetComponent<Button>().onClick.Invoke();
             Debug.Log("HIT");
         }
         Debug.DrawRay(transform.position, forward, Color.blue);
