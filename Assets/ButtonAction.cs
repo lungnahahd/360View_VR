2c675d9f88763925aa45c0117a4a6edce64cab49
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-11 22:39:24 +0900

    360VIew_Ver.1

--- Assets/ButtonAction.cs
+++ Assets/ButtonAction.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using System.Threading;
 
 public class ButtonAction : MonoBehaviour
 {
     public VideoClip clip1;
     public VideoClip clip2;
     public GameObject world;
+    public GameObject button1;
+    public GameObject button2;
     public void changescene(){
         SceneManager.LoadScene("SampleScene");
     }
 
     public void showdance(){
         world.GetComponent<VideoPlayer>().clip = clip2;
-
+        button1.SetActive(false);
+        button2.SetActive(true);
     }
     public void showplace(){
         world.GetComponent<VideoPlayer>().clip = clip1;
+        button1.SetActive(true);
+        button2.SetActive(false);  
     }
 
 }
