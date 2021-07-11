5bc3cbc5024e216cf298b0ef6a837c777964f894
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-11 22:29:16 +0900

    Button으로 Vidoe 전환

--- Assets/ButtonAction.cs
+++ Assets/ButtonAction.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class ButtonAction : MonoBehaviour
 {
+    public VideoClip clip1;
+    public VideoClip clip2;
+    public GameObject world;
     public void changescene(){
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void showdance(){
+        world.GetComponent<VideoPlayer>().clip = clip2;
+
+    }
+    public void showplace(){
+        world.GetComponent<VideoPlayer>().clip = clip1;
+    }
+
 }
